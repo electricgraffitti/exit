f18755babaf291a7e60beec8bd8687914f7167f8
Larry Finn <larry@cube2media.com>
2012-07-27 19:06:07 -0700

    index page text fixes.

--- views/home_content.php
+++ views/home_content.php
@@ -5,7 +5,7 @@
 			<div class="post_copy">
 				<header class="custom_bold section_header">Control the sale of your business. Save thousands.</header>
 				<p>
-					You can't deny that you’ve thought about selling your business. You’ve thought about trading in your 60- or 70-hour weeks to retire and spend your days enjoying the wealth you’ve created. Or, maybe you’re thinking about moving on to your next endeavor.
+					You can't deny that you've thought about selling your business. You've thought about trading in your 60- or 70-hour weeks to retire and spend your days enjoying the wealth you've created. Or, maybe you’re thinking about moving on to your next endeavor.
 				</p>
 				<p>
 					If you've had these thoughts, but don't want to pay a business broker 10% of the sales price, ExitPlan.me is for you.
@@ -18,10 +18,10 @@
 			<div class="post_copy">
 				<header class="custom_bold section_header">Why now is the time to start!</header>
 				<p>
-					As the Baby Boomers retire, more than 70% of the 12 million privately-owned businesses are expected to change hands according to Cornell University economist Robert Avery. The time to sell your business is not after these businesses flood the market – it’s before. It’s now.
+					As the Baby Boomers retire, more than 70% of the 12 million privately-owned businesses are expected to change hands according to Cornell University economist Robert Avery. The time to sell your business is not after these businesses flood the market – it's before. It's now.
 				</p>
 				<p>
-					It usually takes 9-12 months from start to sale, so don't wait until you absolutely must sell. When things get to that point, you’re desperate and will sell for less than you would have if you had planned ahead.  Start the process today!	
+					It usually takes 9-12 months from start to sale, so don't wait until you absolutely must sell. When things get to that point, you're desperate and will sell for less than you would have if you had planned ahead.  Start the process today!	
 				</p>
 			</div>
 			<div class="bgs post_image fold_2"></div>
@@ -55,7 +55,7 @@
 			<div class="post_copy">
 				<header class="custom_bold section_header">Be in control of the change.</header>
 				<p>
-					Let’s face it. Selling your business, whether to retire or to pursue other business opportunities, is a huge life change. Change can be uncomfortable and the decision to sell isn't an easy one. But change is best when it happens on your terms, not someone else’s. You don’t want to have a broker dictate your selling experience – with ExitPlan.me, the sale of your business will happen on YOUR terms.
+					Let's face it. Selling your business, whether to retire or to pursue other business opportunities, is a huge life change. Change can be uncomfortable and the decision to sell isn't an easy one. But change is best when it happens on your terms, not someone else's. You don't want to have a broker dictate your selling experience &mdash; with ExitPlan.me, the sale of your business will happen on YOUR terms.
 				</p>
 			</div>
 			<div class="bgs post_image fold_3"></div>
@@ -64,7 +64,7 @@
 			<div class="post_copy">
 				<header class="custom_bold section_header">Too Good to be true?</header>
 				<p>
-					Does this sound too good to be true? It’s not.  Selling your business will be stressful, exhausting, and a little scary at times. However, we promise you that it will be LESS stressful, LESS tiring, and LESS scary with ExitPlan.me by your side.
+					Does this sound too good to be true? It's not.  Selling your business will be stressful, exhausting, and a little scary at times. However, we promise you that it will be LESS stressful, LESS tiring, and LESS scary with ExitPlan.me by your side.
 				</p>
 			</div>
 			<div class="bgs post_image plane"></div>
